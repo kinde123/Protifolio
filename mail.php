ed4cee9fe4fc8cafdbf6629981388f11b3abd2e6
kinde Tesfaye <kindetadesse5@gmail.com>
2022-01-05 10:29:31 +0100

    code

--- mail.php
+++ mail.php
@@ -2,9 +2,10 @@
 $email = $_POST['email'];
 $message = $_POST['message'];
 $formcontent="From: $name \n Message: $message";
-$recipient = "kindetadesse5@gmail.com";
+$recipient = "kindetgg@gmail.com";
 $subject = "Contact Form";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
-echo "Thank You!";
+
+echo "Thank You!" . " -" . "<a href='form.html' style='text-decoration:none;color:#ff0099;'> Return Home</a>";
 ?>
